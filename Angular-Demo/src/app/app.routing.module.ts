d6e0cd84feb5665d62bb7d89884ef54c9423413b
priyankagupta4398 <priyanka.gupta.sa@gmail.com>
2020-02-17 13:09:51 +0530

    [change]small

--- Angular-Demo/src/app/app.routing.module.ts
+++ Angular-Demo/src/app/app.routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     },
     {
       path: 'addRecipe',
-      // canActivate: [AuthGuard],
+      canActivate: [AuthGuard],
       component: RecipeFormCardComponent,
     },
     {
